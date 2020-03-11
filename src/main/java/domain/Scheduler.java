db498f45b14ff99b161ddba64d830ff98d5177a1
Tanil Orhan <torhan15@ku.edu.tr>
2020-03-11 19:35:20 +0300

    save

--- src/main/java/domain/Scheduler.java
+++ src/main/java/domain/Scheduler.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.List;
+
 public class Scheduler {
 
 
@@ -7,7 +9,7 @@ public class Scheduler {
     //gets a list of courses from user and returns a valid course plan
     public ValidCoursePlan PlanCourses(List<Course> courses){
 
-        ValidCoursePlan coursePlan;
+        ValidCoursePlan coursePlan= new ValidCoursePlan();
         return  coursePlan;
     }
 }
