f37f00253a5e9e9dac3d79569dea8b0a66149992
Tanil Orhan <torhan15@ku.edu.tr>
2020-03-11 19:14:07 +0300

    Student and Departmentclass initialized

--- src/main/java/domain/Course.java
+++ src/main/java/domain/Course.java
@@ -2,8 +2,12 @@ package domain;
 
 public class Course {
 
+
     private String courseCode;   //comp
     private String courseNumber; //302
+    private Boolean hasLab;
+    private Boolean hasPS_DS;
+
 
     public Course(String courseCode,String courseNumber) {
         this.courseCode = courseCode;
@@ -12,6 +16,22 @@ public class Course {
 
     public String getCourseCode() {
         return courseCode;
+    }
+
+    public Boolean getHasLab() {
+        return hasLab;
+    }
+
+    public void setHasLab(Boolean hasLab) {
+        this.hasLab = hasLab;
+    }
+
+    public Boolean getHasPS_DS() {
+        return hasPS_DS;
+    }
+
+    public void setHasPS_DS(Boolean hasPS_DS) {
+        this.hasPS_DS = hasPS_DS;
     }
 
     public void setCourseCode(String courseCode) {
