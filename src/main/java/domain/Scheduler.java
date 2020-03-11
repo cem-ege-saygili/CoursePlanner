55ca17ecd9c525441a0ce0da8f410d3ed7691845
Tanil Orhan <torhan15@ku.edu.tr>
2020-03-11 19:19:27 +0300

    sheduler class initialized

---
+++ src/main/java/domain/Scheduler.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Scheduler {
+
+
+
+    //gets a list of courses from user and returns a valid course plan
+    public ValidCoursePlan PlanCourses(List<Course> courses){
+
+        ValidCoursePlan coursePlan;
+        return  coursePlan;
+    }
+}
