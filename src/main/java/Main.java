c964660b5fc5daa542bbb1e0ca488a6365eb5b5c
naltinbas16 <cibmat-netpyf-cyhTe8>
2020-03-21 19:15:38 +0300

    FIX: Randomly added courses now are successfully sorted before proceeding to the main algo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,10 @@ public class Main {
         Course c1 = new Course("A", 1, 2, 1);
         Course c6 = new Course("F", 11, 14, 5);
 
-
-        cList.add(c1);cList.add(c2);cList.add(c3);cList.add(c4); cList.add(c5); cList.add(c6); cList.add(c7);
+        cList.add(c6);
+        cList.add(c2);cList.add(c7);cList.add(c3);
+        cList.add(c1);
+        cList.add(c4); cList.add(c5);
 
         Scheduler s1 = new Scheduler(cList);
         s1.generateOptimumCoursePlan();
