b690041e3b1284c5c10109d196e05b3585168514
naltinbas16 <cibmat-netpyf-cyhTe8>
2020-04-10 14:47:06 +0300

    isNotOverlappingWith is added for Bundles.

--- src/main/java/domain/CourseBundle.java
+++ src/main/java/domain/CourseBundle.java
@@ -6,11 +6,44 @@ public class CourseBundle {
 
     private String Subject;
     private int Catalog;
-    private List<int[]> timeExtent;
+    private List<int[]> timeExtentList;
 
 //    public int compareTo(CourseBundle b2){
 //        return (this.getEndTimeStamp() < b2.getEndTimeStamp() ? -1 :
 //                (this.getEndTimeStamp() == c2.getEndTimeStamp() ? 0 : 1));
 //    }
+
+    public CourseBundle(String Subject,
+            int Catalog,
+            List<int[]> timeExtentList){
+
+        this.Catalog = Catalog;
+        this.Subject = Subject;
+        this.timeExtentList = timeExtentList;
+
+    }
+
+    private boolean notOverlappingWith(CourseBundle b2){
+        boolean notOverlapping = true;
+        for(int[] thisCurrTimeFrame:this.timeExtentList){
+            for(int[] b2CurrTimeFrame:b2.timeExtentList){
+                int thisCurrTimeFrameStart = thisCurrTimeFrame[0];
+                int thisCurrTimeFrameEnd = thisCurrTimeFrame[1];
+                int b2CurrTimeFrameStart = b2CurrTimeFrame[0];
+                int b2CurrTimeFrameEnd = b2CurrTimeFrame[1];
+
+                boolean isCurrTimeFramesOverlapping = thisCurrTimeFrameEnd >= b2CurrTimeFrameStart &&  b2CurrTimeFrameEnd>=thisCurrTimeFrameEnd    ||
+                                                        thisCurrTimeFrameStart<=b2CurrTimeFrameEnd && b2CurrTimeFrameStart<=thisCurrTimeFrameStart;
+
+                if(isCurrTimeFramesOverlapping){
+                    notOverlapping = false;
+                }
+
+            }
+
+
+        }
+        return notOverlapping;
+    }
 
 }
