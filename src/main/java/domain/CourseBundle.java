f3278872fe9e5ab45aea1f0e2bf6f33bf2fcb842
naltinbas16 <cibmat-netpyf-cyhTe8>
2020-04-10 14:26:22 +0300

    Advanced Scheduler

--- src/main/java/domain/CourseBundle.java
+++ src/main/java/domain/CourseBundle.java
@@ -1,8 +1,16 @@
 package domain;
 
+import java.util.List;
+
 public class CourseBundle {
 
     private String Subject;
     private int Catalog;
+    private List<int[]> timeExtent;
+
+    public int compareTo(CourseBundle b2){
+        return (this.getEndTimeStamp() < b2.getEndTimeStamp() ? -1 :
+                (this.getEndTimeStamp() == c2.getEndTimeStamp() ? 0 : 1));
+    }
 
 }
