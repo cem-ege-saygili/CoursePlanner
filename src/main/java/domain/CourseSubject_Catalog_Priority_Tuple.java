7b217ff9030a0f1cc76ad2fa5a7844ffed9756ce
naltinbas16 <cibmat-netpyf-cyhTe8>
2020-04-12 16:30:44 +0300

    GUI Buttons are now functioning as expected:
    - Add, Remove & Clear buttons are now working.
    
    - Generate Schedule button is currently disabled.
    
    (i.e. waiting for the update for advanced scheduler algorithm)

---
+++ src/main/java/domain/CourseSubject_Catalog_Priority_Tuple.java
@@ -0,0 +1,64 @@
+package domain;
+
+public class CourseSubject_Catalog_Priority_Tuple {
+
+    String subject;
+    int catalog;
+    int priority;
+
+    public CourseSubject_Catalog_Priority_Tuple(
+
+            String subject,
+            int catalog,
+            int priority
+    ){
+        this.priority = priority;
+        this.catalog = catalog;
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(int catalog) {
+        this.catalog = catalog;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        CourseSubject_Catalog_Priority_Tuple t2 = (CourseSubject_Catalog_Priority_Tuple) obj;
+        return ((
+                //this.priority == t2.priority &&
+                this.catalog == t2.catalog &&
+                this.subject.equals(t2.subject))
+                ? true
+                :
+                false);
+    }
+
+    @Override
+    public String toString(){
+
+        return "Subject: " + subject +
+                ", Catalog: " + catalog +
+                ", Priority: " + priority;
+    }
+
+}
