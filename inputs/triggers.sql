28c0279bba625eb1cbd48166b7e53ca6c84a81f2
naltinbas16 <cibmat-netpyf-cyhTe8>
2020-05-12 21:45:28 +0300

    UPDATE DB: All required triggers are now added to triggers.sql
    - DB now includes Log Tables created for the following three tables:
    
         - Courses
         - Classes
         - Instructors
    
    - (UPDATED) DB now includes 13 Triggers as follows:
    
        - For Courses:
          - BEFORE & AFTER DELETE
          - BEFORE & AFTER INSERT
          - BEFORE & AFTER UPDATE
    
        - For Classes
          - BEFORE & AFTER DELETE
          - AFTER INSERT
          - AFTER UPDATE
    
        - For Instructors
          - AFTER DELETE
          - AFTER INSERT
          - AFTER UPDATE
    
    - Processed .db file is also uploaded to remote.

--- inputs/triggers.sql
+++ inputs/triggers.sql
@@ -73,96 +73,6 @@ BEGIN
         WHERE c.CourseId = OLD.CourseId);
 END ;
 
-﻿CREATE TRIGGER trg_before_delete_class
-BEFORE DELETE ON Classes
-WHEN(
-    (1
-    =
-    (SELECT count(DISTINCT cl.ClassId)
-    FROM Instructors i
-    INNER JOIN Class_Instructor_Infos cii on cii.InstructorId = i.InstructorId
-    INNER JOIN Classes cl on cl.ClassId = cii.ClassId
-    WHERE i.InstructorId IN
-        (SELECT cii.InstructorId
-        FROM Class_Instructor_Infos cii
-        INNER JOIN Classes cl ON cii.ClassId = cl.ClassId
-        WHERE cl.ClassId = OLD.ClassId))))
-BEGIN
-    DELETE FROM Instructors
-    WHERE InstructorId IN
-        (SELECT cii.InstructorId
-        FROM Class_Instructor_Infos cii
-        INNER JOIN Classes cl ON cii.ClassId = cl.ClassId
-        WHERE cl.ClassId = OLD.ClassId);
-END ;
-
-CREATE TRIGGER trg_after_delete_courses
-AFTER DELETE ON Courses
-BEGIN
-    INSERT INTO Courses_Log(CoursesLog_DateTime,
-                            CoursesLog_Type,
-                            CoursesLog_CourseId,
-                            CoursesLog_CourseSubject,
-                            CoursesLog_CourseCatalog,
-                            CoursesLog_CourseCareer,
-                            CoursesLog_CourseAcadOrg,
-                            CoursesLog_CourseDescr,
-                            CoursesLog_CourseDescr2)
-    values(datetime('now'),
-           'deleted',
-           OLD.CourseId,
-           OLD.CourseSubject,
-           OLD.CourseCatalog,
-           OLD.CourseCareer,
-           OLD.CourseAcadOrg,
-           OLD.CourseDescr,
-           OLD.CourseDescr2) ;
-END;
-
-CREATE TRIGGER trg_after_delete_classes
-AFTER DELETE ON Classes
-BEGIN
-    INSERT INTO Classes_Log(ClassesLog_DateTime,
-                            ClassesLog_Type,
-                            ClassesLog_ClassId,
-                            ClassesLog_ClassComponent,
-                            ClassesLog_ClassSection,
-                            ClassesLog_ClassMtgStart,
-                            ClassesLog_ClassMtgEnd,
-                            ClassesLog_ClassMon,
-                            ClassesLog_ClassTues,
-                            ClassesLog_ClassWed,
-                            ClassesLog_ClassThurs,
-                            ClassesLog_ClassFri,
-                            ClassesLog_ClassFacilID)
-    values(datetime('now'),
-           'deleted',
-           OLD.ClassId,
-           OLD.ClassComponent,
-           OLD.ClassSection,
-           OLD.ClassMtgStart,
-           OLD.ClassMtgEnd,
-           OLD.ClassMon,
-           OLD.ClassTues,
-           OLD.ClassWed,
-           OLD.ClassThurs,
-           OLD.ClassFri,
-           OLD.ClassFacilID);
-END;
-
-CREATE TRIGGER trg_after_delete_instructors
-AFTER DELETE ON Instructors
-BEGIN
-    INSERT INTO Instructors_Log(InstructorsLog_DateTime,
-                            InstructorsLog_Type,
-                            InstructorsLog_InstructorId,
-                            InstructorsLog_InstructorName)
-    values(datetime('now'),
-           'deleted',
-           OLD.InstructorId,
-           OLD.InstructorName);
-END;
-
 CREATE TRIGGER trg_after_insert_courses
 AFTER INSERT ON Courses
 BEGIN
@@ -259,6 +169,219 @@ BEGIN
     UGRD, MAST, PHD and EEDU');
 END;
 
+CREATE TRIGGER trg_after_delete_courses
+AFTER DELETE ON Courses
+BEGIN
+    INSERT INTO Courses_Log(CoursesLog_DateTime,
+                            CoursesLog_Type,
+                            CoursesLog_CourseId,
+                            CoursesLog_CourseSubject,
+                            CoursesLog_CourseCatalog,
+                            CoursesLog_CourseCareer,
+                            CoursesLog_CourseAcadOrg,
+                            CoursesLog_CourseDescr,
+                            CoursesLog_CourseDescr2)
+    values(datetime('now'),
+           'deleted',
+           OLD.CourseId,
+           OLD.CourseSubject,
+           OLD.CourseCatalog,
+           OLD.CourseCareer,
+           OLD.CourseAcadOrg,
+           OLD.CourseDescr,
+           OLD.CourseDescr2) ;
+END;
+
+﻿CREATE TRIGGER trg_before_delete_class
+BEFORE DELETE ON Classes
+WHEN(
+    (1
+    =
+    (SELECT count(DISTINCT cl.ClassId)
+    FROM Instructors i
+    INNER JOIN Class_Instructor_Infos cii on cii.InstructorId = i.InstructorId
+    INNER JOIN Classes cl on cl.ClassId = cii.ClassId
+    WHERE i.InstructorId IN
+        (SELECT cii.InstructorId
+        FROM Class_Instructor_Infos cii
+        INNER JOIN Classes cl ON cii.ClassId = cl.ClassId
+        WHERE cl.ClassId = OLD.ClassId))))
+BEGIN
+    DELETE FROM Instructors
+    WHERE InstructorId IN
+        (SELECT cii.InstructorId
+        FROM Class_Instructor_Infos cii
+        INNER JOIN Classes cl ON cii.ClassId = cl.ClassId
+        WHERE cl.ClassId = OLD.ClassId);
+END ;
+
+CREATE TRIGGER trg_after_insert_classes
+AFTER INSERT ON Classes
+BEGIN
+    INSERT INTO Classes_Log(ClassesLog_DateTime,
+                            ClassesLog_Type,
+                            ClassesLog_ClassId,
+                            ClassesLog_ClassComponent,
+                            ClassesLog_ClassSection,
+                            ClassesLog_ClassMtgStart,
+                            ClassesLog_ClassMtgEnd,
+                            ClassesLog_ClassMon,
+                            ClassesLog_ClassTues,
+                            ClassesLog_ClassWed,
+                            ClassesLog_ClassThurs,
+                            ClassesLog_ClassFri,
+                            ClassesLog_ClassFacilID)
+    values(datetime('now'),
+           'inserted',
+           NEW.ClassId,
+           NEW.ClassComponent,
+           NEW.ClassSection,
+           NEW.ClassMtgStart,
+           NEW.ClassMtgEnd,
+           NEW.ClassMon,
+           NEW.ClassTues,
+           NEW.ClassWed,
+           NEW.ClassThurs,
+           NEW.ClassFri,
+           NEW.ClassFacilID) ;
+END ;
+
+CREATE TRIGGER trg_after_update_classes
+AFTER UPDATE ON Classes
+BEGIN
+INSERT INTO Classes_Log(ClassesLog_DateTime,
+                            ClassesLog_Type,
+                            ClassesLog_ClassId,
+                            ClassesLog_ClassComponent,
+                            ClassesLog_ClassSection,
+                            ClassesLog_ClassMtgStart,
+                            ClassesLog_ClassMtgEnd,
+                            ClassesLog_ClassMon,
+                            ClassesLog_ClassTues,
+                            ClassesLog_ClassWed,
+                            ClassesLog_ClassThurs,
+                            ClassesLog_ClassFri,
+                            ClassesLog_ClassFacilID)
+    values(datetime('now'),
+           'updated_OLD_values',
+           OLD.ClassId,
+           OLD.ClassComponent,
+           OLD.ClassSection,
+           OLD.ClassMtgStart,
+           OLD.ClassMtgEnd,
+           OLD.ClassMon,
+           OLD.ClassTues,
+           OLD.ClassWed,
+           OLD.ClassThurs,
+           OLD.ClassFri,
+           OLD.ClassFacilID) ;
+    INSERT INTO Classes_Log(ClassesLog_DateTime,
+                            ClassesLog_Type,
+                            ClassesLog_ClassId,
+                            ClassesLog_ClassComponent,
+                            ClassesLog_ClassSection,
+                            ClassesLog_ClassMtgStart,
+                            ClassesLog_ClassMtgEnd,
+                            ClassesLog_ClassMon,
+                            ClassesLog_ClassTues,
+                            ClassesLog_ClassWed,
+                            ClassesLog_ClassThurs,
+                            ClassesLog_ClassFri,
+                            ClassesLog_ClassFacilID)
+    values(datetime('now'),
+           'updated_NEW_values',
+           NEW.ClassId,
+           NEW.ClassComponent,
+           NEW.ClassSection,
+           NEW.ClassMtgStart,
+           NEW.ClassMtgEnd,
+           NEW.ClassMon,
+           NEW.ClassTues,
+           NEW.ClassWed,
+           NEW.ClassThurs,
+           NEW.ClassFri,
+           NEW.ClassFacilID) ;
+END ;
+
+CREATE TRIGGER trg_after_delete_classes
+AFTER DELETE ON Classes
+BEGIN
+    INSERT INTO Classes_Log(ClassesLog_DateTime,
+                            ClassesLog_Type,
+                            ClassesLog_ClassId,
+                            ClassesLog_ClassComponent,
+                            ClassesLog_ClassSection,
+                            ClassesLog_ClassMtgStart,
+                            ClassesLog_ClassMtgEnd,
+                            ClassesLog_ClassMon,
+                            ClassesLog_ClassTues,
+                            ClassesLog_ClassWed,
+                            ClassesLog_ClassThurs,
+                            ClassesLog_ClassFri,
+                            ClassesLog_ClassFacilID)
+    values(datetime('now'),
+           'deleted',
+           OLD.ClassId,
+           OLD.ClassComponent,
+           OLD.ClassSection,
+           OLD.ClassMtgStart,
+           OLD.ClassMtgEnd,
+           OLD.ClassMon,
+           OLD.ClassTues,
+           OLD.ClassWed,
+           OLD.ClassThurs,
+           OLD.ClassFri,
+           OLD.ClassFacilID);
+END;
+
+CREATE TRIGGER trg_after_delete_instructors
+AFTER DELETE ON Instructors
+BEGIN
+    INSERT INTO Instructors_Log(InstructorsLog_DateTime,
+                            InstructorsLog_Type,
+                            InstructorsLog_InstructorId,
+                            InstructorsLog_InstructorName)
+    values(datetime('now'),
+           'deleted',
+           OLD.InstructorId,
+           OLD.InstructorName);
+END;
+
+CREATE TRIGGER trg_after_insert_instructors
+AFTER INSERT ON Instructors
+BEGIN
+    INSERT INTO Instructors_Log(InstructorsLog_DateTime,
+                            InstructorsLog_Type,
+                            InstructorsLog_InstructorId,
+                            InstructorsLog_InstructorName)
+    values(datetime('now'),
+           'inserted',
+           NEW.InstructorId,
+           NEW.InstructorName);
+END;
+
+CREATE TRIGGER trg_after_update_instructors
+AFTER UPDATE ON Instructors
+BEGIN
+    INSERT INTO Instructors_Log(InstructorsLog_DateTime,
+                            InstructorsLog_Type,
+                            InstructorsLog_InstructorId,
+                            InstructorsLog_InstructorName)
+    values(datetime('now'),
+           'updated_OLD_values',
+           OLD.InstructorId,
+           OLD.InstructorName);
+
+    INSERT INTO Instructors_Log(InstructorsLog_DateTime,
+                            InstructorsLog_Type,
+                            InstructorsLog_InstructorId,
+                            InstructorsLog_InstructorName)
+    values(datetime('now'),
+           'updated_NEW_values',
+           NEW.InstructorId,
+           NEW.InstructorName);
+END;
+
 END TRANSACTION;
 
 ﻿DELETE FROM Courses where CourseCareer <> 'UGRD' or CourseSubject = 'ISP';
@@ -307,3 +430,58 @@ order by
 SUBSTR("Time of Class", -2, 2), Ordering,
 LENGTH("Time of Class")
 );
+
+select
+SUBSTR(i.InstructorName, INSTR(i.InstructorName, ',') + 1, LENGTH(i.InstructorName) - INSTR(i.InstructorName, ','))
+|| ' ' ||
+SUBSTR(i.InstructorName, 0, INSTR(i.InstructorName, ','))
+as "Course Instructor First & Last Name",
+c.CourseSubject || ' ' || c.CourseCatalog as "Course Name",
+c.CourseCareer as "Course Level",
+c.CourseAcadOrg as "Course Faculty",
+c.CourseDescr as "Course Description"
+from Classes cl
+inner join Class_Course_Infos cci on cci.ClassId = cl.ClassId
+inner join Courses c on c.CourseId = cci.CourseId
+inner join Class_Instructor_Infos cii on cii.ClassId = cl.ClassId
+inner join Instructors i on cii.InstructorId = i.InstructorId
+where i.InstructorName like '%NIKOS';
+
+CREATE VIEW CoursesInformation_of_Instructors_Whose_FirstAndLastNamesStartingWithLetterA AS
+SELECT
+SUBSTR(i.InstructorName, INSTR(i.InstructorName, ',') + 1, LENGTH(i.InstructorName) - INSTR(i.InstructorName, ','))
+|| ' ' ||
+SUBSTR(i.InstructorName, 0, INSTR(i.InstructorName, ','))
+as "Course Instructor First & Last Name",
+c.CourseSubject || ' ' || c.CourseCatalog as "Course Name",
+c.CourseCareer as "Course Level",
+c.CourseAcadOrg as "Course Faculty",
+c.CourseDescr as "Course Description"
+from Classes cl
+inner join Class_Course_Infos cci on cci.ClassId = cl.ClassId
+inner join Courses c on c.CourseId = cci.CourseId
+inner join Class_Instructor_Infos cii on cii.ClassId = cl.ClassId
+inner join Instructors i on cii.InstructorId = i.InstructorId
+where
+(SUBSTR
+(i.InstructorName,
+INSTR(i.InstructorName, ',') + 1,
+LENGTH(i.InstructorName) - INSTR(i.InstructorName, ','))
+like
+'A%')
+AND
+(SUBSTR
+(i.InstructorName,
+0,
+INSTR(i.InstructorName, ','))
+like
+'A%')
+AND
+(cii.ClassInstructorRole = 'SI' or cii.ClassInstructorRole = 'PI')
+group by c.CourseId
+order by "Course Instructor First & Last Name", "Course Name", "Course Description";
+--Create a view for the following query:
+	--Show instructor name in first&last fashion, course level, course faculty, course description
+	--Show the entries for the instructors who not only assume the role of either PI or SI
+	--but also have their first and last names starting with letter 'A'
+	--group same courses together
